53f81280ec61bad0ad8fd709a3bf88e96f9a3745
rhitverse <rohitsainii.work@gmail.com>
2026-03-19 17:00:09 +0530

    fix

--- lib/screens/settings/profile_screen.dart
+++ lib/screens/settings/profile_screen.dart
@@ -164,7 +164,7 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
                         if (image == null &&
                             (user.profilePic.isEmpty ||
                                 user.profilePic ==
-                                    'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/573323465_1219825463302212_7278921664109726296_n.png?stp=dst-jpg_e0_s150x150_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLmRqYW5nby4xNTAuYzIifQ&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=1&_nc_oc=Q6cZ2QH-D4ZS7uKSql_7HpxPaexdEIDjMMfhNvl9oU4gByxeIGsh_5gJXnszJMFR51u0JMs&_nc_ohc=2tyZ8HJj7tUQ7kNvwFR5mRI&_nc_gid=GRpEBnRVsBgnwu3BMSgGcQ&edm=AGqCYasBAAAA&ccb=7-5&ig_cache_key=YW5vbnltb3VzX3Byb2ZpbGVfcGlj.3-ccb7-5&oh=00_AfwDar6o1e1hMOGViptl34FG0-BsdSVP7vcySd3H1JcG6g&oe=69B5FAEA&_nc_sid=6c5dea')) {
+                                    'https://yt3.ggpht.com/a/default-user=s600-k-no-rp-mo')) {
                           return;
                         }
 
